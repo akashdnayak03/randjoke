5f89418dcafe3815cf39ba40198c22f32855b176
akashdnayak03 <akashdnayak31@gmail.com>
2025-07-29 22:42:04 +0530

    remove h3

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ document.getElementById("btn").addEventListener("click", () => {
         .json()
         .then((data) => {
           console.log(data);
-          if(data.type ==="single"){
+          if (data.type === "single") {
             document.querySelector("h1").innerText = data.joke;
-          } 
-          else if(data.type==="twopart"){
-          document.querySelector("h1").innerText = data.setup;
-          document.querySelector("h3").innerText = data.delivery;
+            document.querySelector("h3").innerText = "";
+          } else if (data.type === "twopart") {
+            document.querySelector("h1").innerText = data.setup;
+            document.querySelector("h3").innerText = data.delivery;
           }
         })
         .catch((er) => {
