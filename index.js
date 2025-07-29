29b2f583361f2bb260d329b4df768308eb737ebf
akashdnayak03 <akashdnayak31@gmail.com>
2025-07-29 19:10:33 +0530

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+let url = "https://sv443.net/jokeapi/v2/joke/Any";
+document.getElementById("btn").addEventListener("click", () => {
+  fetch(url)
+    .then((res) => {
+      console.log(res);
+      res
+        .json()
+        .then((data) => {
+          console.log(data);
+
+          document.querySelector("h1").innerText = data.setup;
+          document.querySelector("h3").innerText = data.delivery;
+        })
+        .catch((er) => {
+          console.log(er);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
